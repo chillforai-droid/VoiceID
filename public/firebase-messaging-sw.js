42f83514bc6193c1ea2a68842dd4c7cfda004ec4
Texly <chillforai@gmail.com>
2026-08-31 14:15:39 +0530

    Update Firebase configuration with real credentials

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -15,12 +15,12 @@ importScripts("https://www.gstatic.com/firebasejs/12.0.0/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/12.0.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "REPLACE_WITH_FIREBASE_API_KEY",
-  authDomain: "REPLACE_WITH_FIREBASE_AUTH_DOMAIN", // e.g. your-project.firebaseapp.com
-  projectId: "REPLACE_WITH_FIREBASE_PROJECT_ID",
-  storageBucket: "REPLACE_WITH_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "REPLACE_WITH_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "REPLACE_WITH_FIREBASE_APP_ID",
+  apiKey: "AIzaSyDHDQIXS96v4KrDkolMWpnrKUSLiZhJyQg",
+  authDomain: "voiceid-2d259.firebaseapp.com",
+  projectId: "voiceid-2d259",
+  storageBucket: "voiceid-2d259.firebasestorage.app",
+  messagingSenderId: "104607436012",
+  appId: "1:104607436012:web:e60b078a52421cfe88e9c9",
 });
 
 const messaging = firebase.messaging();
